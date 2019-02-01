afab4caeda6c1673843f6c31d0fa3d99c3a8b63b
Valentin Churavy <v.churavy@gmail.com>
2019-01-31 21:13:17 -0500

    force inline the heck out of everything

--- src/context.jl
+++ src/context.jl
@@ -15,8 +15,18 @@
 
 using Cassette
 
+function transform(ctx, ref)
+    ci = ref.code_info
+    noinline = any(@nospecialize(x) -> Core.Compiler.isexpr(x, :meta) && x.args[1] == :noinline, ci.code)
+    if !noinline
+        ci.inlineable = true
+    end
+    return ci
+end
+const InlinePass = Cassette.@pass transform
+
 Cassette.@context CUDACtx
-const cudactx = Cassette.disablehooks(CUDACtx())
+const cudactx = Cassette.disablehooks(CUDACtx(pass = InlinePass))
 
 # libdevice.jl
 for f in (:cos, :cospi, :sin, :sinpi, :tan,
@@ -36,5 +46,4 @@ for f in (:cos, :cospi, :sin, :sinpi, :tan,
 end
 
 contextualize(f::F) where F = (args...) -> Cassette.overdub(cudactx, f, args...)
-
 
